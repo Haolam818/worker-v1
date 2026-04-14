f154ed13957e10e1582ccbc715c0e22383a508c6
Haolam818 <huanghaolam@gmail.com>
2026-04-14 20:01:57 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: './', // 设置根目录
-  build: {
-    outDir: 'dist', // 告诉 Vercel 编译后的东西放在这里
-  }
+  plugins: [react()],
+  base: './'
 })
